af57366024d5a73a8c6ab3e2406ddf4b66ead8a4
SimonSomlai <simon_somlai@hotmail.com>
2017-02-10 19:18:18 +0100

    updated

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+environment_variables = File.join(Rails.root, 'config', 'initializers', 'environment_variables.rb')
+load(environment_variables) if File.exists?(environment_variables) 
