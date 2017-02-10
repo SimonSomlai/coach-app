626a4b307496b5973e67b0c75588e93fcd107aa4
SimonSomlai <simon_somlai@hotmail.com>
2017-02-10 22:12:46 +0100

    final update je pense

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,14 +18,14 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-   config.serve_static_assets = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.serve_static_assets = true
+  config.public_file_server.enabled = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -57,7 +57,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "coach-app_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.public_file_server.enabled = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
