97242785080db66e7e7c4ec6a9740a912c1fb077
SimonSomlai <simon_somlai@hotmail.com>
2017-02-10 20:37:11 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 require_relative 'boot'
-require 'carrierwave'
-require 'carrierwave/orm/activerecord'
-require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +7,6 @@ Bundler.require(*Rails.groups)
 
 module CoachApp
   class Application < Rails::Application
-    
+
   end
 end
