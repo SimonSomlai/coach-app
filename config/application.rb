dc2aa20df727dddf6fb6359e38b3caa336be7d41
SimonSomlai <simon_somlai@hotmail.com>
2017-02-10 20:42:29 +0100

    updated rails app

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative 'boot'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
